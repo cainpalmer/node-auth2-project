62508df17cf6be094a10c969b1b4b21b5ab7e6d6
cainpalmer <cain.a.palmer@gmail.com>
2021-12-14 15:01:08 -0500

    findBy's fixed

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -5,14 +5,17 @@ const restricted = (req, res, next) => {
 }
 
 const only = role_name => (req, res, next) => {
+  next()
 }
 
 
 const checkUsernameExists = (req, res, next) => {
+  next()
 }
 
 
 const validateRoleName = (req, res, next) => {
+  next()
 }
 
 module.exports = {
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -7,9 +7,17 @@ function find() {
 }
 
 function findBy(filter) {
+  return db('users')
+  .join('roles', 'users.role_id', 'roles.role_id')
+  .select('user_id', 'username', 'password', 'role_name')
+  .where(filter)
 }
 
 function findById(user_id) {
+  return db('users')
+  .join('roles', 'users.role_id', 'roles.role_id')
+  .select('user_id', 'username', 'role_name')
+  .where('users.user_id', user_id).first()
 }
 
 async function add({ username, password, role_name }) { // done for you
